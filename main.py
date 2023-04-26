71bfe4ba25f1a24454ba27d7d44cfbadbe01bee4
Denver31 <daniil.igoshev05@gmail.com>
2023-04-25 22:15:10 -0300

    add interface

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame
-import time
 
+pygame.init()
 H = 600
 W = 500
-FIELD_H = W - 50
-FIELD_W = H - 150
+FIELD_H = 450
+FIELD_W = 450
 FPS = 30
 
 clock = pygame.time.Clock()
@@ -20,15 +20,28 @@ display = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Game")
 
 # создание поля
-display.fill(BACKGROUND_COLOR)
 field_sub = pygame.Surface((FIELD_W, FIELD_H))
-field_sub.fill(BACKGROUND_COLOR)
 field_rect = field_sub.get_rect()
 field_rect.center = (W // 2, H // 2 + 50)
-pygame.draw.line(field_sub, BLACK, (FIELD_W // 3, 0), (FIELD_W // 3, FIELD_H), 3)
-pygame.draw.line(field_sub, BLACK, (2 * FIELD_W // 3, 0), (2 * FIELD_W // 3, FIELD_H), 3)
-pygame.draw.line(field_sub, BLACK, (0, FIELD_H // 3), (FIELD_W, FIELD_H // 3), 3)
-pygame.draw.line(field_sub, BLACK, (0, 2 * FIELD_H // 3), (FIELD_W, 2 * FIELD_H // 3), 3)
+
+text_sub = pygame.Surface((450, 120))
+text_rect = (240, 50)
+
+font = pygame.font.SysFont('arial', 60)
+text1 = font.render("Ходят X", True, BLACK, BACKGROUND_COLOR)
+text2 = font.render("Ходят O", True, BLACK, BACKGROUND_COLOR)
+text3 = font.render("Победил X", True, BLACK, BACKGROUND_COLOR)
+text4 = font.render("Победил O", True, BLACK, BACKGROUND_COLOR)
+text5 = font.render("Ничья", True, BLACK, BACKGROUND_COLOR)
+
+
+def draw_field():
+    display.fill(BACKGROUND_COLOR)
+    field_sub.fill(BACKGROUND_COLOR)
+    pygame.draw.line(field_sub, BLACK, (FIELD_W // 3, 0), (FIELD_W // 3, FIELD_H), 5)
+    pygame.draw.line(field_sub, BLACK, (2 * FIELD_W // 3, 0), (2 * FIELD_W // 3, FIELD_H), 5)
+    pygame.draw.line(field_sub, BLACK, (0, FIELD_H // 3), (FIELD_W, FIELD_H // 3), 5)
+    pygame.draw.line(field_sub, BLACK, (0, 2 * FIELD_H // 3), (FIELD_W, 2 * FIELD_H // 3), 5)
 
 
 def inputPlayer(player, m, pos):
@@ -48,33 +61,39 @@ def win(m):
             if m[0][i] != 0:
                 pygame.draw.line(field_sub, RED, (l // 2 + l * i, l // 2), (l // 2 + l * i, l // 2 + l * 2), 5)
                 show(m[0][i], m)
-                time.sleep(2)
             return m[0][i]
         elif m[i][0] == m[i][1] == m[i][2]:
             if m[i][0] != 0:
                 pygame.draw.line(field_sub, RED, (l // 2, l // 2 + l * i), (l // 2 + 2 * l, l // 2 + l * i), 5)
                 show(m[i][0], m)
-                time.sleep(2)
             return m[i][0]
         elif m[0][0] == m[1][1] == m[2][2]:
             if m[0][0] != 0:
                 pygame.draw.line(field_sub, RED, (l // 2, l // 2), (l // 2 + 2 * l, l // 2 + 2 * l), 5)
                 show(m[0][0], m)
-                time.sleep(2)
             return m[0][0]
         elif m[0][2] == m[1][1] == m[2][0]:
             if m[0][2] != 0:
-                pygame.draw.line(field_sub, RED, (l // 2 + 2 * l, l // 2 + 2 * l), (l // 2, l // 2),  5)
+                pygame.draw.line(field_sub, RED, (l // 2, l // 2 + 2 * l), (l // 2 + 2 * l, l // 2), 5)
                 show(m[0][2], m)
-                time.sleep(2)
             return m[0][2]
+        elif sum([row.count(0) for row in m]) == 0:
+            return 3
     return 0
 
 
 def show(player, m):
     """Отображает текущее состояние поля"""
-    display.fill(BLACK)
+    display.fill(BACKGROUND_COLOR)
     display.blit(field_sub, field_rect)
+
+    text_sub.fill(BACKGROUND_COLOR)
+    if player == 1:
+        text_sub.blit(text2, text2.get_rect(center=text_rect))
+    else:
+        text_sub.blit(text1, text1.get_rect(center=text_rect))
+    display.blit(text_sub, text_sub.get_rect(center=text_rect))
+
     for i in range(3):
         for j in range(3):
             if m[i][j] == 1:  # отрисовка крестиков
@@ -86,16 +105,35 @@ def show(player, m):
                 pygame.draw.circle(field_sub, BLACK, (l // 2 + l * j, l // 2 + l * i), (l // 2 - 10), 7)
 
     pygame.display.update()
-    # print("ходит игрок", player + 1)
 
 
-def startgame():
+def endGame(winner):
+    text_sub.fill(BACKGROUND_COLOR)
+    if winner == 1:
+        text_sub.blit(text3, text3.get_rect(center=text_rect))
+    elif winner == 2:
+        text_sub.blit(text4, text4.get_rect(center=text_rect))
+    else:
+        text_sub.blit(text5, text5.get_rect(center=text_rect))
+
+    display.blit(text_sub, text_sub.get_rect(center=(250, 50)))
+    pygame.display.update()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    startGame()
+
+
+def startGame():
     """Запускает игру,
         игрок ставит крестик или нолик в свободную клетку,
         проверяется на наличие закрытой линии,
     """
-    game_run = True
-
+    draw_field()
     field = [[0] * 3 for _ in range(3)]
     player = 1
     while win(field) == 0:
@@ -108,11 +146,12 @@ def startgame():
                         mouse_pos = pygame.mouse.get_pos()
                         mouse_pos = (mouse_pos[0] - field_rect.left, mouse_pos[1] - field_rect.top)
                         field, player = inputPlayer(player, field, mouse_pos)
-
         show(player, field)
         clock.tick(FPS)
-    print(f"Игра завершена \nпобедил {win(field)} игрок")
+
+    show(player, field)
+    endGame(win(field))
 
 
 if __name__ == "__main__":
-    startgame()
+    startGame()
